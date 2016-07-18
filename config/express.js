7a12414ffbcb34f02a2f9017a2468406fb81b6d7
tarungarg546 <tarungarg546@gmail.com>
2016-07-19 01:11:09 +0530

    fix: Address in use error fixed by changing port :unicorn:

--- config/express.js
+++ config/express.js
@@ -3,13 +3,14 @@ const init = express => {
 
   var express = express || require('express');
   const bodyParser = require('body-parser');
+  const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.set('views', __dirname+'/../views');
   app.set('view engine', 'ejs');
   app.use(express.static(__dirname+'/../static'));
-  app.set('port', process.env.PORT || 8080);
+  app.set('port', process.env.PORT || 8000);
   
   return app;
 
