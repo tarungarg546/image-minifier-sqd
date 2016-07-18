92652d8372c658eff38f972420af984162430523
tarungarg546 <tarungarg546@gmail.com>
2016-07-19 01:49:21 +0530

    Feat: Basic Animation and tab with respective content showing :white_check_mark: :fire:

---
+++ static/js/app.js
@@ -0,0 +1,64 @@
+class App {
+  constructor() {
+    this.tabContainer = document.querySelector('.tab-list');
+
+    this.cleanActiveTabs = this.cleanActiveTabs.bind(this);
+    this.showTab = this.showTab.bind(this);
+    this.showContent = this.showContent.bind(this);
+
+    this.showTab(this.tabContainer.querySelector('.active'))
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    //Use event bubbling because attaching event handler to one DOM element is cheaper than
+    //to attach it on multiples __ tradeoff :- memory usage
+    this.tabContainer.addEventListener('click',ev => {
+      const target = ev.target;
+
+      if(target.nodeName === 'LI') {
+        this.showTab(target);
+        target.classList.add('active');
+      }
+    });
+  }
+
+  /**
+   * [showTab Show content of target tab and clean other active tabs]
+   * @param  {[HTMLElement]} target
+   * @return {[type]}        [description]
+   */
+  showTab(target) {
+    this.cleanActiveTabs(); // clean other active tabs
+    
+    if(!target.classList.contains('active')) {
+      target.classList.add('active'); // make this tab active
+    }
+    const targetContent = document.querySelector(`.${target.dataset.content}`)
+
+    return this.showContent(targetContent); // show content of corresponding tab
+
+  }
+
+  cleanActiveTabs() {
+    const tabList = Array.from(this.tabContainer.querySelectorAll('li'));
+
+    tabList.forEach(node => {
+      node.classList.remove('active');
+    })
+  }
+
+  showContent(target) {
+    const contentList = Array.from(document.querySelectorAll('.tab-content'));
+
+    contentList.forEach(node => {
+      if(node !== target) {
+        node.classList.remove('show');
+      } else {
+        node.classList.add('show');
+      }
+    })
+  }
+}
+
+new App();
